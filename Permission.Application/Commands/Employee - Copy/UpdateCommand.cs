7f02bd70d71b816dfd6d102aa8188a3f4d5132db
sohan <sohan@ACRLHO-WS135>
2025-12-23 11:34:15 +0600

    check repo

---
+++ Permission.Application/Commands/Employee - Copy/UpdateCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Permission.Core.Entities.EmployeeEntity;
+using Permission.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permission.Application.Commands.Employee
+{
+    public record UpdateCommand(int id,TBLEmployeeEntity Entity) : IRequest<TBLEmployeeEntity>;
+    public class UpdateCommandHandler(IEmplolyeeRepository repository) : IRequestHandler<UpdateCommand, TBLEmployeeEntity>
+    {
+        public async Task<TBLEmployeeEntity> Handle(UpdateCommand request, CancellationToken cancellation)
+        {
+            return await repository.UpdateData(request.id,request.Entity);
+        }
+    }
+}

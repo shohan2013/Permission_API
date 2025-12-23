f4493d5d491fc739da87d661c65d829d345980ff
sohan <sohan@ACRLHO-WS135>
2025-12-23 11:42:56 +0600

    remove error

--- Permission.UI/Controllers/MasterDetails/MasterDetailsController.cs
+++ Permission.UI/Controllers/MasterDetails/MasterDetailsController.cs
@@ -12,41 +12,6 @@ namespace Permission.Api.Controllers.MasterDetails
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Add([FromBody] TBLEmployeeEntity Employee)
-        {
-            var result = await sender.Send(new AddCommand(Employee));
-            return Ok(result);
-        }
-
-
-
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var result = await sender.Send(new GetData());
-            return Ok(result);
-        }
-
-        [HttpGet("GetDataByID/{id}")]
-        public async Task<IActionResult> GetDataByID(int id)
-        {
-            var result = await sender.Send(new GetDataByID(id));
-            return Ok(result);
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> Update(int id, [FromBody] TBLEmployeeEntity Entity)
-        {
-            var result = await sender.Send(new UpdateCommand(id, Entity));
-            return Ok(result);
-        }
-
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var result = await sender.Send(new DeleteCommand(id));
-            return Ok(result);
-        }
+       
     }
 }
